3e060cdfda00799bbe586f8330463ab9c501b45f
Harish Kumar <harish.kumar@pkware.com>
2022-03-09 15:02:59 +0530

    Successfully fetched data from api

--- my-assignments/day2-fetch/script.js
+++ my-assignments/day2-fetch/script.js
@@ -1,19 +1,13 @@
-
 async function getData() {
-    // url = "https://api.github.com/users";
-    // fetch(url).then((response) => {
-    //     return response.json();
-    // }).then((data) => {
-    //     console.log(data);
-    // })
-    try {
-        const response = await fetch("https://api.github.com/users");
-        const users = await response.json();
-      console.log(users);
-    
-    }
- catch (error) {
+
+  try {
+    const response = await fetch("https://api.github.com/users");
+    const users = await response.json();
+    console.log(users);
+
+  }
+  catch (error) {
     console.log(error.message);
-}
+  }
 }
 getData();
