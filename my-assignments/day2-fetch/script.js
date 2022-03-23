d20a041256b3c26318e97dc333aece44fcbd6477
Harish Kumar <harish.kumar@pkware.com>
2022-03-23 20:24:32 +0530

    Displayed data fetch from api into table

--- my-assignments/day2-fetch/script.js
+++ my-assignments/day2-fetch/script.js
@@ -3,11 +3,24 @@ async function getData() {
   try {
     const response = await fetch("https://api.github.com/users");
     const users = await response.json();
-    console.log(users);
 
+    return users;
   }
   catch (error) {
     console.log(error.message);
   }
 }
-getData();
+getData().then((data) => {
+  // console.log(data[0].login);
+  let tabledata = "";
+  data.map((value) => {
+    tabledata +=
+      `<tr>
+    <td>${value.login}</td>
+    <td>${value.id}</td>
+    <td><a href='${value.html_url}'>${value.html_url}</a></td>
+    <td><img src='${value.avatar_url}'/></td>
+  </tr>`
+  });
+  document.getElementById("table_body").innerHTML = tabledata;
+})
